741641e40b1c265f974b1fdf9b06afab15b27990
TEYI <tonysson7@gmail.com>
2019-08-17 08:23:56 +0300

    premier commit,installation et page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends Controller{
+
+    /**
+     * @Route("/", name="homepage")
+     * 
+     */
+
+    public function home(){
+        return $this->render(
+            'home.html.twig'
+        );
+
+    }
+
+}
